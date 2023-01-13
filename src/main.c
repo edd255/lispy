d3316fdc134a30c9b793e0cb40ec6d2d9e1b5fb0
edd255 <101655959+edd255@users.noreply.github.com>
2023-01-13 15:42:28 +0100

    feat: evaluate expressions

--- src/main.c
+++ src/main.c
@@ -4,6 +4,38 @@
 #include <editline/readline.h>
 #include "../libs/mpc.h"
 
+long eval_op(long x, char* op, long y)
+{
+        if (strcmp(op, "+") == 0) {
+                return x + y;
+        }
+        if (strcmp(op, "-") == 0) {
+                return x - y;
+        }
+        if (strcmp(op, "*") == 0) {
+                return x * y;
+        }
+        if (strcmp(op, "/") == 0) {
+                return x / y;
+        }
+        return 0;
+}
+
+long eval(mpc_ast_t* ast)
+{
+        if (strstr(ast -> tag, "number")) {
+                return atoi(ast -> contents);
+        }
+        char* op = ast -> children[1] -> contents;
+        long x = eval(ast -> children[2]);
+        int i = 3;
+        while (strstr(ast -> children[i] -> tag, "expr")) {
+                x = eval_op(x, op, eval(ast -> children[i]));
+                i++;
+        }
+        return x;
+}
+
 int main()
 {
         mpc_parser_t* number   = mpc_new("number");
@@ -27,11 +59,17 @@ int main()
 
         while (true) {
                 char* input = readline("> ");
+                if (strncmp(input, "exit", 4) == 0) {
+                    return 0;
+                }
+
                 (void) add_history(input);
 
                 mpc_result_t r;
                 if (mpc_parse("<stdin>", input, clisp, &r)) {
                         mpc_ast_print(r.output);
+                        long result = eval(r.output);
+                        printf("%li\n", result);
                         mpc_ast_delete(r.output);
                 } else {
                         mpc_err_print(r.error);
