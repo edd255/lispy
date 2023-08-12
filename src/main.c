8bb0605fa504c690b525d51b3078dbdc551a089c
edd255 <abbadon@disroot.org>
2023-08-12 17:29:29 +0200

    chore: added editline lib again

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "printing.h"
 #include "reading.h"
 #include "values.h"
+#include <editline/readline.h>
 
 //=== DECLARATIONS =============================================================
 //--- Functions ----------------------------------------------------------------
@@ -36,22 +37,6 @@ static const char* const usages[] = {
     "lispy --filename=<FILENAME> --nostdlib --write_logs ",
     NULL,
 };
-
-//--- Readline replacement -----------------------------------------------------
-static char buffer[2048];
-
-char* readline(char* prompt) {
-    fputs(prompt, stdout);
-    fgets(buffer, 2048, stdin);
-    char* cpy = malloc(strlen(buffer) + 1);
-    strcpy(cpy, buffer);
-    cpy[strlen(cpy) - 1] = '\0';
-    return cpy;
-}
-
-void add_history(char* unused) {
-    UNUSED(unused);
-}
 
 //=== MAIN METHOD ==============================================================
 
