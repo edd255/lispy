e07e16c41b06bf7ebe21e26b35f94202fa006154
edd255 <abbadon@disroot.org>
2023-08-31 13:35:10 +0200

    fix(if): let if accept and eval sexpr

--- src/builtins/conditional.c
+++ src/builtins/conditional.c
@@ -1,5 +1,6 @@
 #include "builtins/conditional.h"
 
+#include "builtins/list.h"
 #include "eval.h"
 #include "io.h"
 
@@ -9,7 +10,7 @@ lval_t* builtin_if(lenv_t* env, lval_t* args) {
     assert(NULL != args);
     UNUSED(env);
     LCHECK_NUM(__func__, args, 3);
-    LCHECK_TYPE(__func__, args, 0, LISPY_VAL_NUM);
+    LCHECK_TYPES(__func__, args, 0, LISPY_VAL_NUM, LISPY_VAL_SEXPR);
     LCHECK_TYPE(__func__, args, 1, LISPY_VAL_QEXPR);
     LCHECK_TYPE(__func__, args, 2, LISPY_VAL_QEXPR);
 
@@ -18,7 +19,15 @@ lval_t* builtin_if(lenv_t* env, lval_t* args) {
     args->cell[1]->type = LISPY_VAL_SEXPR;
     args->cell[2]->type = LISPY_VAL_SEXPR;
 
-    if (args->cell[0]->num) {
+    lval_t* cond = lval_eval(env, args->cell[0]);
+    LCHECK(
+        cond,
+        cond->type == LISPY_VAL_NUM,
+        "%s: symbolic expression did not evaluate to number",
+        __func__
+    );
+
+    if (cond->num) {
         // If condition is true evaluate first expression
         lval_t* y = lval_pop(args, 1);
         assert(NULL != y);
