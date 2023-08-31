e43fca7095fca8511403687a7a7be66c15b72910
edd255 <abbadon@disroot.org>
2023-08-31 11:57:43 +0200

    docs(list/strings): added docs for fst, snd, trd, last

--- docs/html/list_8h.js
+++ docs/html/list_8h.js
@@ -2,12 +2,16 @@ var list_8h =
 [
     [ "builtin_cons", "list_8h.html#aa1c7a9f0d82393536ba9404cc12c93be", null ],
     [ "builtin_eval", "list_8h.html#a917b114e551a2a650f24b283b0d3d0ab", null ],
+    [ "builtin_first", "list_8h.html#a7f611fe2e0b5ad41dfedf784b3bbd25e", null ],
     [ "builtin_head", "list_8h.html#a15737f64eba6006e7e636c1d2c1c69ed", null ],
     [ "builtin_join", "list_8h.html#ab73a5212ed790048ee4311b191376173", null ],
+    [ "builtin_last", "list_8h.html#a7a6d464742d7be6831ada74466a9be80", null ],
     [ "builtin_len", "list_8h.html#a812c1dd7c6785868671eab5e465cc7d8", null ],
     [ "builtin_list", "list_8h.html#ad84560815b65cb42f45f3ab8615252dd", null ],
     [ "builtin_nth", "list_8h.html#a1c5c6ffaf7af0e8490a8aa4e33b25433", null ],
     [ "builtin_pack", "list_8h.html#a82fbe755c307f2454755b4dcce1de673", null ],
+    [ "builtin_second", "list_8h.html#a824eaad8a2cc3a33b68dd03263af22e3", null ],
     [ "builtin_tail", "list_8h.html#a0c718446878069eb87cef4208d032e50", null ],
+    [ "builtin_third", "list_8h.html#ae2465a6323fd0980ed2c3a5435f15b2a", null ],
     [ "builtin_unpack", "list_8h.html#af693588e2c80ae046141d7d778085956", null ]
 ];
