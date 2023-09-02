d89c6dd9da1b364f3575e1642bd129a2632bb909
edd255 <abbadon@disroot.org>
2023-09-02 08:55:56 +0200

    chore(deps): updated color-sh deps

--- deps/color-sh/color-sh.c
+++ deps/color-sh/color-sh.c
@@ -1,25 +1,24 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "color-sh.h"
 
-#if defined(_WIN32)
+#include <stdarg.h>
+#include <stdio.h>
 
+#if HAS_WIN
     void set_color(int color) {
-        if(!out)
+        if (!out) {
             out = GetStdHandle(STD_OUTPUT_HANDLE);
-
+        }
         SetConsoleTextAttribute(out, FOREGROUND_INTENSITY | color);
     }
-
-#elif defined(__linux__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__unix__)
-    void set_color(char *color) {
-        printf(color);
+#elif HAS_NIX
+    void set_color(char* color) {
+        printf("%s", color);
     }
 #else
     #error Platform not supported by color-sh lib
-#endif /* _WIN32 */
+#endif
 
-void color_red(char *fmt, ...) {
+void color_red(char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
     set_color(RED);
@@ -28,27 +27,7 @@ void color_red(char *fmt, ...) {
     set_color(REWIND);
 }
 
-void color_bred(char *fmt, ...) {
-    #if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__unix__)
-        va_list ap;
-        va_start(ap, fmt);
-        set_color(RED_BOLD);
-        vprintf(fmt, ap);
-        va_end(ap);
-        set_color(REWIND);
-    #elif defined(_WIN32)
-        va_list ap;
-        va_start(ap, fmt);
-        set_color(RED);
-        vprintf(fmt, ap);
-        va_end(ap);
-        set_color(REWIND);
-    #else
-    #error Platform not supported by color-sh lib
-    #endif
-}
-
-void color_green(char *fmt, ...) {
+void color_green(char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
     set_color(GREEN);
@@ -57,27 +36,7 @@ void color_green(char *fmt, ...) {
     set_color(REWIND);
 }
 
-void color_bgreen(char *fmt, ...) {
-    #if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__unix__)
-        va_list ap;
-        va_start(ap, fmt);
-        set_color(GREEN_BOLD);
-        vprintf(fmt, ap);
-        va_end(ap);
-        set_color(REWIND);
-    #elif defined(_WIN32)
-        va_list ap;
-        va_start(ap, fmt);
-        set_color(GREEN);
-        vprintf(fmt, ap);
-        va_end(ap);
-        set_color(REWIND);
-    #else
-    #error Platform not supported by color-sh lib
-    #endif
-}
-
-void color_white(char *fmt, ...) {
+void color_white(char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
     set_color(WHITE);
@@ -86,15 +45,91 @@ void color_white(char *fmt, ...) {
     set_color(REWIND);
 }
 
-void color_bwhite(char *fmt, ...) {
-    #if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__unix__)
+void color_blue(char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    set_color(BLUE);
+    vprintf(fmt, ap);
+    va_end(ap);
+    set_color(REWIND);
+}
+
+void color_purple(char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    set_color(PURPLE);
+    vprintf(fmt, ap);
+    va_end(ap);
+    set_color(REWIND);
+}
+
+void color_yellow(char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    set_color(YELLOW);
+    vprintf(fmt, ap);
+    va_end(ap);
+    set_color(REWIND);
+}
+
+void color_cyan(char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    set_color(CYAN);
+    vprintf(fmt, ap);
+    va_end(ap);
+    set_color(REWIND);
+}
+
+void color_bred(char* fmt, ...) {
+    #if HAS_NIX
+        va_list ap;
+        va_start(ap, fmt);
+        set_color(RED_BOLD);
+        vprintf(fmt, ap);
+        va_end(ap);
+        set_color(REWIND);
+    #elif HAS_WIN
+        va_list ap;
+        va_start(ap, fmt);
+        set_color(RED);
+        vprintf(fmt, ap);
+        va_end(ap);
+        set_color(REWIND);
+    #else
+        #error Platform not supported by color-sh lib
+    #endif
+}
+
+void color_bgreen(char* fmt, ...) {
+    #if HAS_NIX
+        va_list ap;
+        va_start(ap, fmt);
+        set_color(GREEN_BOLD);
+        vprintf(fmt, ap);
+        va_end(ap);
+        set_color(REWIND);
+    #elif HAS_WIN
+        va_list ap;
+        va_start(ap, fmt);
+        set_color(GREEN);
+        vprintf(fmt, ap);
+        va_end(ap);
+        set_color(REWIND);
+    #else
+        #error Platform not supported by color-sh lib
+    #endif
+}
+
+void color_bwhite(char* fmt, ...) {
+    #if HAS_NIX
         va_list ap;
         va_start(ap, fmt);
         set_color(WHITE_BOLD);
         vprintf(fmt, ap);
         va_end(ap);
         set_color(REWIND);
-    #elif defined(_WIN32)
+    #elif HAS_WIN
         va_list ap;
         va_start(ap, fmt);
         set_color(WHITE);
@@ -106,24 +141,15 @@ void color_bwhite(char *fmt, ...) {
     #endif
 }
 
-void color_blue(char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    set_color(BLUE);
-    vprintf(fmt, ap);
-    va_end(ap);
-    set_color(REWIND);
-}
-
-void color_bblue(char *fmt, ...) {
-    #if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__unix__)
+void color_bblue(char* fmt, ...) {
+    #if HAS_NIX
         va_list ap;
         va_start(ap, fmt);
         set_color(BLUE_BOLD);
         vprintf(fmt, ap);
         va_end(ap);
         set_color(REWIND);
-    #elif defined(_WIN32)
+    #elif HAS_WIN
         va_list ap;
         va_start(ap, fmt);
         set_color(BLUE);
@@ -135,25 +161,15 @@ void color_bblue(char *fmt, ...) {
     #endif
 }
 
-void color_purple(char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    set_color(PURPLE);
-    vprintf(fmt, ap);
-    va_end(ap);
-    set_color(REWIND);
-}
-
-void color_bpurple(char *fmt, ...)
-{
-    #if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__unix__)
+void color_bpurple(char* fmt, ...) {
+    #if HAS_NIX
         va_list ap;
         va_start(ap, fmt);
         set_color(PURPLE_BOLD);
         vprintf(fmt, ap);
         va_end(ap);
         set_color(REWIND);
-    #elif defined(_WIN32)
+    #elif HAS_WIN
         va_list ap;
         va_start(ap, fmt);
         set_color(PURPLE);
@@ -165,24 +181,15 @@ void color_bpurple(char *fmt, ...)
     #endif
 }
 
-void color_yellow(char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    set_color(YELLOW);
-    vprintf(fmt, ap);
-    va_end(ap);
-    set_color(REWIND);
-}
-
-void color_byellow(char *fmt, ...) {
-    #if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__unix__)
+void color_byellow(char* fmt, ...) {
+    #if HAS_NIX
         va_list ap;
         va_start(ap, fmt);
         set_color(YELLOW_BOLD);
         vprintf(fmt, ap);
         va_end(ap);
         set_color(REWIND);
-    #elif defined(_WIN32)
+    #elif HAS_WIN
         va_list ap;
         va_start(ap, fmt);
         set_color(YELLOW);
@@ -194,24 +201,15 @@ void color_byellow(char *fmt, ...) {
     #endif
 }
 
-void color_cyan(char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    set_color(CYAN);
-    vprintf(fmt, ap);
-    va_end(ap);
-    set_color(REWIND);
-}
-
-void color_bcyan(char *fmt, ...) {
-    #if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__unix__)
+void color_bcyan(char* fmt, ...) {
+    #if HAS_NIX
         va_list ap;
         va_start(ap, fmt);
         set_color(CYAN_BOLD);
         vprintf(fmt, ap);
         va_end(ap);
         set_color(REWIND);
-    #elif defined(_WIN32)
+    #elif HAS_WIN
         va_list ap;
         va_start(ap, fmt);
         set_color(CYAN);
@@ -226,4 +224,3 @@ void color_bcyan(char *fmt, ...) {
 void color_rewind(void) {
     set_color(REWIND);
 }
-
