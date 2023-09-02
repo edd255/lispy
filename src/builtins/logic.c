6418ef5eb011da311c70155fb704c94d1fd62099
edd255 <abbadon@disroot.org>
2023-09-02 13:24:20 +0200

    refactor(logic): simplified builtin_logic

--- src/builtins/logic.c
+++ src/builtins/logic.c
@@ -14,32 +14,20 @@ lval_t* builtin_logic(lenv_t* env, lval_t* args, char* op) {
             LCHECK_NUM(op, args, 2);
             LCHECK_TYPE(op, args, 0, LISPY_VAL_NUM);
             LCHECK_TYPE(op, args, 1, LISPY_VAL_NUM);
-            if ((0 == args->cell[0]->num) || (0 == args->cell[1]->num)) {
-                result = 0;
-            } else {
-                result = 1;
-            }
+            result = !((0 == args->cell[0]->num) || (0 == args->cell[1]->num));
             break;
         }
         case LISPY_LOGIC_OR: {
             LCHECK_NUM(op, args, 2);
             LCHECK_TYPE(op, args, 0, LISPY_VAL_NUM);
             LCHECK_TYPE(op, args, 1, LISPY_VAL_NUM);
-            if ((0 == args->cell[0]->num) && (0 == args->cell[1]->num)) {
-                result = 0;
-            } else {
-                result = 1;
-            }
+            result = !((0 == args->cell[0]->num) && (0 == args->cell[1]->num));
             break;
         }
         case LISPY_LOGIC_NOT: {
             LCHECK_NUM(op, args, 1);
             LCHECK_TYPE(op, args, 0, LISPY_VAL_NUM);
-            if (0 == args->cell[0]->num) {
-                result = 1;
-            } else {
-                result = 0;
-            }
+            result = (0 == args->cell[0]->num);
             break;
         }
         case LISPY_LOGIC_UNKNOWN: {
