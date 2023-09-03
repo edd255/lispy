05f7885db98c1fcf2780cfe02297578cefbeafec
edd255 <abbadon@disroot.org>
2023-09-03 20:59:42 +0200

    docs(strings): added docs for len member in lval strings

--- docs/html/search/variables_6.js
+++ docs/html/search/variables_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['lispy_0',['lispy',['../main_8c.html#a065721c05a37f8befd463c37f3d3dfe4',1,'main.c']]]
+  ['len_0',['len',['../structlval__t.html#a67c584e34b0f761989e35958db1721dc',1,'lval_t']]],
+  ['lispy_1',['lispy',['../main_8c.html#a065721c05a37f8befd463c37f3d3dfe4',1,'main.c']]]
 ];
--- docs/html/structlval__t.js
+++ docs/html/structlval__t.js
@@ -8,6 +8,7 @@ var structlval__t =
     [ "env", "structlval__t.html#af979706beafa540d8e534f4dfdaad6ea", null ],
     [ "err", "structlval__t.html#a409c993597657ae29b862c851c30257f", null ],
     [ "formals", "structlval__t.html#a276d8c0d4a7a004ff2ba1d1c23c1ccdc", null ],
+    [ "len", "structlval__t.html#a67c584e34b0f761989e35958db1721dc", null ],
     [ "num", "structlval__t.html#a5c0c66219f00c006607afd3291f389cd", null ],
     [ "str", "structlval__t.html#adfeaf11648b10794aa44bb246c465d0c", null ],
     [ "sym", "structlval__t.html#ab81f6c077b0b5083f8ae2fdc0834c753", null ],
