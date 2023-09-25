998e077adc9721d207d44c0aeb28d92adeb88d5e
edd255 <abaddon@disroot.org>
2023-09-25 21:09:32 +0200

    feat(prompt): read last history file

--- src/utils/git_info.h
+++ src/utils/git_info.h
@@ -3,7 +3,7 @@
 #ifndef LISPY_UTILS_GIT_INFO
 #define LISPY_UTILS_GIT_INFO
 
-#define LISPY_GIT_COMMIT_HASH "5654475"
+#define LISPY_GIT_COMMIT_HASH "fd6e486"
 #define LISPY_GIT_BRANCH_NAME "main"
 
 #endif
