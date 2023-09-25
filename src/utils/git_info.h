367f19e3c535b2c5288be7736283923ce1c945fe
edd255 <101655959+edd255@users.noreply.github.com>
2023-09-25 21:05:05 +0200

    fix: save last elements in history

--- src/utils/git_info.h
+++ src/utils/git_info.h
@@ -3,7 +3,7 @@
 #ifndef LISPY_UTILS_GIT_INFO
 #define LISPY_UTILS_GIT_INFO
 
-#define LISPY_GIT_COMMIT_HASH "212a3fc"
+#define LISPY_GIT_COMMIT_HASH "5654475"
 #define LISPY_GIT_BRANCH_NAME "main"
 
 #endif
